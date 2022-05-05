ce6fa5d9740b167bb1b11d72f08acf6aa582216b
calvin3oo <86371905+calvin3oo@users.noreply.github.com>
2022-05-04 20:27:25 -0600

    fixed date format

--- weather/js/allPages.js
+++ weather/js/allPages.js
@@ -1,4 +1,10 @@
-document.getElementById('last-updated-time').innerHTML = new Date().toDateString();
+const today = new Date();
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+
+//format: "Tuesday, 2 May 2022"
+document.getElementById('last-updated-time').innerHTML = `${weekday[today.getDay()]}, ${today.getDate()} ${month[today.getMonth()]} ${today.getFullYear()}`;
 
 document.getElementById('current-year').innerHTML = new Date().getFullYear();
 
