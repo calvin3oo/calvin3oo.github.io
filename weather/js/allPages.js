4ab222a740fc48d9c510aff0560b3e8c6dbd6fcc
calvin3oo <86371905+calvin3oo@users.noreply.github.com>
2022-05-04 23:20:02 -0600

    finished preston page

--- weather/js/allPages.js
+++ weather/js/allPages.js
@@ -2,12 +2,23 @@ const today = new Date();
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
+const dayOfWeek = weekday[today.getDay()];
 
 //format: "Tuesday, 2 May 2022"
-document.getElementById('last-updated-time').innerHTML = `${weekday[today.getDay()]}, ${today.getDate()} ${month[today.getMonth()]} ${today.getFullYear()}`;
+document.getElementById('last-updated-time').innerHTML = `${dayOfWeek}, ${today.getDate()} ${month[today.getMonth()]} ${today.getFullYear()}`;
 
 document.getElementById('current-year').innerHTML = new Date().getFullYear();
 
 const toggleMenu = () => {
     document.getElementById('nav-ul').classList.toggle('responsive');
 }
+
+const makeCurrentNavActive = () => {
+    const tempURL = window.location.href.split('/');
+    const lastURLSection = tempURL[tempURL.length-1];
+    const navName = lastURLSection.substring(0, lastURLSection.indexOf('.html'));
+
+    document.getElementById(navName).classList.add('active');
+}
+
+makeCurrentNavActive();
