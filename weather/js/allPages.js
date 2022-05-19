cd2b4e512b6296f84a953958f8011d6ce79a7e4d
calvin3oo <86371905+calvin3oo@users.noreply.github.com>
2022-05-18 22:33:49 -0600

    home page

--- weather/js/allPages.js
+++ weather/js/allPages.js
@@ -1,29 +1,28 @@
+//Date stuff
 const today = new Date();
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
 const dayOfWeek = weekday[today.getDay()];
 
-//format: "Tuesday, 2 May 2022"
-document.getElementById('last-updated-time').innerHTML = `${dayOfWeek}, ${today.getDate()} ${month[today.getMonth()]} ${today.getFullYear()}`;
+//What page is it on?
+const tempURL = window.location.href.split('/');
+const lastURLSection = tempURL[tempURL.length-1];
+const navName = lastURLSection.substring(0, lastURLSection.indexOf('.html'));
 
+//Format the Footer's Date
+document.getElementById('last-updated-time').innerHTML = `${dayOfWeek}, ${today.getDate()} ${month[today.getMonth()]} ${today.getFullYear()}`;
 document.getElementById('current-year').innerHTML = new Date().getFullYear();
 
-if(dayOfWeek==="Friday"){
+//format Navbar area
+if(navName==='preston' && dayOfWeek==="Friday"){
     document.getElementById('onFriday').classList.add('show');
 }
 
+//NavBar Stuff:
 const toggleMenu = () => {
     document.getElementById('nav-ul').classList.toggle('responsive');
 }
-
 const makeCurrentNavActive = () => {
-    const tempURL = window.location.href.split('/');
-    const lastURLSection = tempURL[tempURL.length-1];
-    const navName = lastURLSection.substring(0, lastURLSection.indexOf('.html'));
-    console.log(navName);
-
     document.getElementById(navName).classList.add('active');
 }
-
 makeCurrentNavActive();
